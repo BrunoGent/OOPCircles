6bda610f3208cfa33371b345c602c5f1199358be
Bruno Gentiletti <brunogentiletti@gmail.com>
2023-01-15 23:48:22 +0000

    Commited

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        boolean flag;
         Scanner reader = new Scanner(System.in);
         Circle GreenCircle = new Circle(10.5,"Green");
 
@@ -11,8 +12,20 @@ public class Main {
 
 
         //insert radius
+
         System.out.println("insert Radius");
-        GreenCircle.setRadius(Double.parseDouble(reader.nextLine()));
+        do {
+            try {
+                GreenCircle.setRadius(Double.parseDouble(reader.nextLine()));
+                flag = true;
+            }
+            catch (Exception e) {
+                System.out.println("Input not valid, enter a number");
+                flag = false;
+            }
+
+        }
+        while (!flag);
 
 
         System.out.println("Radius is " + GreenCircle.getRadius());
